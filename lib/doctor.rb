d9e97a66d08b7da52a6ea42cef6347f77da5eaa0
Matt Eichsteadt and Skye Atwood <matt@>
2016-12-06 10:10:22 -0800

    add save method and equals method for doctor class

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -3,18 +3,34 @@ class Doctor
   def initialize(attributes)
     @name = attributes[:name]
     @speciality = attributes[:speciality]
+    @id = attributes[:id]
   end
 
   def self.all
     returned_doctors = []
     doctors = DB.exec("SELECT * FROM doctors;")
     doctors.each do |doctor|
-      name = doctor[:name]
-      speciality = doctor[:speciality]
-      id = doctor[:id].to_i
+      name = doctor['name']
+      speciality = doctor['speciality']
+      id = doctor['id'].to_i
       new_doctor = Doctor.new({name: name, speciality: speciality, id: id})
       returned_doctors.push(new_doctor)
     end
     returned_doctors
+  end
+
+  def attributes
+    @attributes
+  end
+
+  def save
+    DB.exec("INSERT INTO doctors (name, speciality) VALUES ('#{@name}', '#{@speciality}');")
+  end
+
+  define_method(:==) do |other_doctor|
+    booleans = []
+      booleans.push(self.name.==(other_doctor.name))
+      booleans.push(self.speciality.==(other_doctor.speciality))
+    booleans.all?
   end
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -7,4 +7,20 @@ describe 'Doctor' do
       expect(Doctor.all).to eq([])
     end
   end
+
+  describe '#save' do
+    it 'will save a new doctor to the database' do
+      new_doctor = Doctor.new({name: "Carlos", speciality: "Rhinoplasty", id: nil})
+      new_doctor.save
+      expect(Doctor.all).to eq([new_doctor])
+    end
+  end
+
+  describe "#=="  do
+    it "is the same task if it has the same attributes" do
+      task1 = Doctor.new({:name => "Carlos", :speciality => 'Rhinoplasty'})
+      task2 = Doctor.new({:name => "Carlos", :speciality => 'Rhinoplasty'})
+      expect(task1).to(eq(task2))
+    end
+  end
 end
